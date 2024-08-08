5cc4852cb6695de6a577e2c3b3b951f49e94b589
PCL-TUM <phichet.pcl@gmail.com>
2024-08-08 14:46:29 +0700

    Add: fonts and setup route

--- src/App.vue
+++ src/App.vue
@@ -1,120 +1,25 @@
 <script setup>
-  import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
+  <div class="flex flex-col p-10 gap-5">
 
-  <!-- Container -->
-     <div class="flex items-end justify-center min-h-screen">
-       <div class="flex flex-col w-full">
-           <!-- Navigation Bar -->
-           <div class="px-5 pt-3 bg-white shadow-lg rounded-2xl rounded-bl-none rounded-br-none">
-             <div class="flex flex-row space-x-3 justify-center">
-               <!-- Item #1 -->
-            <div class="flex group">
-              <a href="#" class="p-3 text-gray-400 hover:text-yellow-500">
-                <span class="flex flex-col items-center">
-                  <!-- Icon -->
-                  <i class="mdi mdi-home-outline mdi-24px mx-1 text-gray-500 group-hover:text-gray-700
-                  transition-color duration-200"></i>
-                  
-                  <!-- Text -->
-                  <span class="text-xs mb-2 transition-all duration-200">
-                    Home
-                  </span>
-                  
-                  <!-- Focus Dot -->
-                  <span class="h-2 w-2 rounded-full group-hover:bg-yellow-500
-                  transition-all duration-150 delay-100"></span>
-                </span>
-              </a>
-            </div>
-            
-            <!-- Item #2 -->
-            <div class="flex group">
-              <a href="#" class="p-3 text-gray-400 hover:text-yellow-500">
-                <span class="flex flex-col items-center">
-                  <!-- Icon -->
-                  <i class="mdi mdi-compass-outline mdi-24px mx-1 text-gray-500 group-hover:text-gray-700
-                  transition-color duration-200"></i>
-                  
-                  <!-- Text -->
-                  <span class="text-xs mb-2 transition-all duration-200">
-                    Explore
-                  </span>
-                  
-                  <!-- Focus Dot -->
-                  <span class="h-2 w-2 rounded-full group-hover:bg-yellow-500
-                    transition-all duration-150 delay-100"></span>
-                  </span>
-              </a>
-            </div>
-
-            <!-- Item #3 Active -->
-            <div class="flex group">
-              <a href="#" class="p-3 text-yellow-500 hover:text-yellow-500">
-                <span class="flex flex-col items-center">
-                  <!-- Icon -->
-                  <i class="mdi mdi-layers-outline mdi-24px mx-1 text-gray-700 group-hover:text-gray-700
-                  transition-color duration-200"></i>
-                  
-                  <!-- Text -->
-                  <span class="text-xs mb-2 transition-all duration-200">
-                    Feeds
-                  </span>
-                  
-                  <!-- Focus Dot -->
-                  <span class="h-1 w-5 rounded-full bg-yellow-500 group-hover:bg-yellow-500
-                  hover:h-3 hover:w-3 transition-all duration-150 delay-100"></span>
-                </span>
-              </a>
-            </div>
-
-            <!-- Item #4 -->
-            <div class="flex group">
-              <a href="#" class="p-3 text-gray-400 hover:text-yellow-500">
-                <span class="flex flex-col items-center">
-                  <!-- Icon -->
-                  <i class="mdi mdi-basket-plus-outline mdi-24px mx-1 text-gray-500 group-hover:text-gray-700
-                  transition-color duration-200"></i>
-                  
-                  <!-- Text -->
-                  <span class="text-xs mb-2 transition-all duration-200">
-                    Cart
-                  </span>
-                  
-                  <!-- Focus Dot -->
-                  <span class="h-2 w-2 rounded-full group-hover:bg-yellow-500
-                  transition-all duration-150 delay-100"></span>
-                </span>
-              </a>
-            </div>
-            
-            <!-- Item #5 -->
-            <div class="flex group">
-              <a href="#" class="p-3 text-gray-400 hover:text-yellow-500">
-                <span class="flex flex-col items-center">
-                  <!-- Icon -->
-                  <i class="mdi mdi-account-circle-outline mdi-24px mx-1 text-gray-500 group-hover:text-gray-700
-                  transition-color duration-200"></i>
-                  
-                  <!-- Text -->
-                  <span class="text-xs mb-2 transition-all duration-200">
-                    Account
-                  </span>
-                  
-                  <!-- Focus Dot -->
-                  <span class="h-2 w-2 rounded-full group-hover:bg-yellow-500
-                  transition-all duration-150 delay-100"></span>
-                </span>
-              </a>
-            </div>
-          </div>
-        </div>
-    </div>
+    <h1>Hello App!</h1>
+  
+    <p>
+      <strong>Current route path:</strong> {{ $route.fullPath }}
+    </p>
+  
+    <nav>
+      <RouterLink to="/">Go to Home</RouterLink>
+      <RouterLink to="/about">Go to About</RouterLink>
+    </nav>
+  
+    <main>
+      <RouterView />
+    </main>
+  
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
